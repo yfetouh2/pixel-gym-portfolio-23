1103f9eb1d2193d143acb9ba834cded262169e11
Youssef AbouElFetouh <ys634721@dal.ca>
2026-02-10 03:32:57 -0400

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,6 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   base:  "/pixel-gym-portfolio-23/",
-  server: {
-    host: "::",
-    port: 8080,
-  },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
